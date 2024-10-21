2435db34b1e9c9c3d8d064d16036eefc718cdbc8
beatrizgdc <beatrizgdcevaio@gmail.com>
2024-10-21 17:58:56 -0300

    feat: conexao MongoDB

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
 //Configuração e inicialização do servidor
+import express from 'express';
+import { connectDB } from './config/db';
+
+const app = express();
+const port = 4000;
+
+connectDB();
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,1 +1,16 @@
 //Conexão com o MongoDB
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO_URI;
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoURI!);
+        console.log('Conexão com Banco de Dados bem sucedida!');
+    } catch (err) {
+        console.log('Erro conexão Banco de Dados:', err);
+    }
+};
